4c66722af0bc968578747e99148b8eeec355c905
amir <amirrezazadeh.job@gmail.com>
2025-04-11 14:40:09 +0330

    add navbar all size

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 let menu = document.querySelector("#nav-menu");
-menu.addEventListener('click' , e=>{    
-    if(e.target.id=='nav-menu'){
-        manageMenu()
-    }
-})
-function manageMenu() {        
-  if (menu.style.right == '0px') {
-      closeMenu();
-    } else {
-        openMenu();
+menu.addEventListener("click", (e) => {
+  if (e.target.id == "nav-menu") {
+    manageMenu();
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  let theme = window.localStorage.getItem("theme");
+  switchTheme(theme);
+});
+function manageMenu() {
+  if (menu.style.right == "0px") {
+    closeMenu();
+  } else {
+    openMenu();
   }
 }
 function closeMenu() {
@@ -26,5 +31,25 @@ function toggleMenu(el) {
   } else {
     el.parentElement.parentElement.style.height = h + "px";
     el.style.transform = "rotate(-90deg)";
+  }
+}
+
+function switchTheme(text) {
+  let circle = document.querySelector("#switch-theme-circle");
+  if (circle.classList.contains("translate-x-7.5") || text == "dark") {
+    circle.classList.remove("translate-x-7.5");
+    applyTheme("dark");
+    window.localStorage.setItem("theme", "dark");
+  } else {
+    circle.classList.add("translate-x-7.5");
+    applyTheme("light");
+    window.localStorage.setItem("theme", "light");
+  }
+}
+function applyTheme(text) {
+  if (text == "dark") {
+    console.log(text);
+  } else {
+    console.log(text);
   }
 }
